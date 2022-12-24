a535d320a817b16ca522f89cd662cbec2fab7b0a
Omer Erdem <m.o.erdem@outlook.com>
2022-12-24 19:56:57 +0100

    snapshot

--- src/views/Dashboard/NewPlaylist.vue
+++ src/views/Dashboard/NewPlaylist.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="p-4">
         <h1 class="text-center text-2xl mb-4">New Playlist</h1>
-
+        <input type="text" class="block w-full rounded-md">
     </section>
 </template>
 
